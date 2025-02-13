713316c078c3729b9d5211e206ba713f7d13bab4
Tirth <tirthjani2227@gmail.com>
2025-02-13 18:48:42 +0530

    Check are Same Tree

--- daily practice/SameTree.java
+++ daily practice/SameTree.java
@@ -1,6 +1,28 @@
-import DepthOfBTree.Node;
 
 public class SameTree {
+    static class Node{
+        int data ;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            right=null;
+            left=null;
+        }
+    }
+    static class BinaryTree{
+        static int i=-1;
+        public static Node build(int[] arr){
+            i++;
+            if(arr[i]==-1){
+                return null;
+            }
+            Node newNode = new Node(arr[i]);
+            newNode.left=build(arr);
+            newNode.right=build(arr);
+            return newNode;
+        }
+    }
     public static void main(String[] args) {
         
     }
