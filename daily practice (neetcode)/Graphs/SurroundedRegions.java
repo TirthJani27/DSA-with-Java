8218ef1a3d7aa83d380e6b1faed1a70a4f51712c
Tirth <tirthjani2227@gmail.com>
2025-05-11 22:43:33 +0530

    Added a graph solution

--- daily practice (neetcode)/Graphs/SurroundedRegions.java
+++ daily practice (neetcode)/Graphs/SurroundedRegions.java
@@ -18,23 +18,23 @@ public class SurroundedRegions {
         int[][] help = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
         for (int i = 0; i < board.length; i++) {
             if (board[i][0] == 'O' && !vis[i][0]) {
-            q.add(new Pair(i, 0));
-            vis[i][0] = true;
+                q.add(new Pair(i, 0));
+                vis[i][0] = true;
             }
             if (board[i][board[0].length - 1] == 'O' && !vis[i][board[0].length - 1]) {
-            q.add(new Pair(i, board[0].length - 1));
-            vis[i][board[0].length - 1] = true;
+                q.add(new Pair(i, board[0].length - 1));
+                vis[i][board[0].length - 1] = true;
             }
         }
 
         for (int j = 0; j < board[0].length; j++) {
             if (board[0][j] == 'O' && !vis[0][j]) {
-            q.add(new Pair(0, j));
-            vis[0][j] = true;
+                q.add(new Pair(0, j));
+                vis[0][j] = true;
             }
             if (board[board.length - 1][j] == 'O' && !vis[board.length - 1][j]) {
-            q.add(new Pair(board.length - 1, j));
-            vis[board.length - 1][j] = true;
+                q.add(new Pair(board.length - 1, j));
+                vis[board.length - 1][j] = true;
             }
         }
 
@@ -43,21 +43,21 @@ public class SurroundedRegions {
             int x = curr.x;
             int y = curr.y;
             for (int k = 0; k < help.length; k++) {
-            int currX = help[k][0] + x;
-            int currY = help[k][1] + y;
-            if (currX >= 0 && currX < vis.length && currY >= 0 && currY < vis[0].length
-                && !vis[currX][currY] && board[currX][currY] == 'O') {
-                q.add(new Pair(currX, currY));
-                vis[currX][currY] = true;
-            }
+                int currX = help[k][0] + x;
+                int currY = help[k][1] + y;
+                if (currX >= 0 && currX < vis.length && currY >= 0 && currY < vis[0].length
+                        && !vis[currX][currY] && board[currX][currY] == 'O') {
+                    q.add(new Pair(currX, currY));
+                    vis[currX][currY] = true;
+                }
             }
         }
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-            if (board[i][j] == 'O' && !vis[i][j]) {
-                board[i][j] = 'X';
-            }
+                if (board[i][j] == 'O' && !vis[i][j]) {
+                    board[i][j] = 'X';
+                }
             }
         }
     }
